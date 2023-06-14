a40cca92427430863d60c274f854869deb0ed2e5
이승일 <ever9415@gmail.com>
2023-06-14 16:29:56 +0900

    이승일: gnb 2deps 개발 완료

--- lg-global/brand-story/layout.js
+++ lg-global/brand-story/layout.js
@@ -1,3 +1,8 @@
+function getElementIndex(element) {
+    return [].indexOf.call(element.parentNode.children, element);
+}
+
+
 function initCommonHeader() {
     var cHeader = document.querySelector("#header.c-header");
     var chPosTop = cHeader.offsetTop - (document.documentElement.scrollTop || window.scrollY);
@@ -50,6 +55,65 @@ function initCommonHeader() {
                 },300)
             }
         })
+    }
+}
+
+function initGnbDeps2(tAnchor) {
+    if(window.innerWidth <= 768) {
+        return false;
+    }
+
+    var tAnchor = tAnchor;
+    var tItem = tAnchor.parentNode;
+    var tList = tItem.parentNode;
+    var tDeps2 = tItem.querySelector(".c-header__gnb__list__item__deps2");
+    var tDeps2Menu = tDeps2.querySelector(".c-header__gnb__list__item__deps2__menu");
+    var tDeps2MenuItem = tDeps2Menu.querySelectorAll(".c-header__gnb__list__item__deps2__menu__item");
+    var tDeps2MenuItemOrgIndex = getElementIndex(document.querySelector(".c-header__gnb__list__item__deps2__menu__item__anchor--active").parentNode);
+    var tDeps2Visual = tDeps2.querySelector(".c-header__gnb__list__item__deps2__visual");
+    var tDeps2VisualItem = tDeps2Visual.querySelectorAll(".c-header__gnb__list__item__deps2__visual__item");
+        tDeps2VisualItem[tDeps2MenuItemOrgIndex].classList.add("c-header__gnb__list__item__deps2__visual__item--active")
+
+    if(tAnchor.className.indexOf("--active") < 0) {
+        tList.querySelectorAll(".c-header__gnb__list__item").forEach(function(restItem) {
+            restItem.querySelector("a").classList.remove("c-header__gnb__list__item__anchor--active")
+        })
+        tAnchor.classList.add("c-header__gnb__list__item__anchor--active")
+    }
+
+    if(tAnchor.className.indexOf("--on") < 0) {
+        tList.querySelectorAll(".c-header__gnb__list__item").forEach(function(restItem) {
+            restItem.querySelector("a").classList.remove("c-header__gnb__list__item__anchor--on")
+        })
+        tAnchor.classList.add("c-header__gnb__list__item__anchor--on")
+    }
+
+    tDeps2MenuItem.forEach(function(t2mi,i) {
+        t2mi.querySelector("a").onmouseenter = function() {
+            tDeps2MenuItem.forEach(function(rest,j) {
+                rest.querySelector("a").classList.remove("c-header__gnb__list__item__deps2__menu__item__anchor--active")
+                tDeps2VisualItem[j].classList.remove("c-header__gnb__list__item__deps2__visual__item--active")
+            })
+            t2mi.querySelector("a").classList.add("c-header__gnb__list__item__deps2__menu__item__anchor--active")
+            tDeps2VisualItem[i].classList.add("c-header__gnb__list__item__deps2__visual__item--active")
+        }
+    })
+
+    tDeps2Menu.onmouseleave = function() {
+        tDeps2MenuItem.forEach(function(rest,k) {
+            rest.querySelector("a").classList.remove("c-header__gnb__list__item__deps2__menu__item__anchor--active")
+            tDeps2VisualItem[k].classList.remove("c-header__gnb__list__item__deps2__visual__item--active")
+        })
+        tDeps2MenuItem[tDeps2MenuItemOrgIndex].querySelector("a").classList.add("c-header__gnb__list__item__deps2__menu__item__anchor--active")
+        tDeps2VisualItem[tDeps2MenuItemOrgIndex].classList.add("c-header__gnb__list__item__deps2__visual__item--active")
+
+    }
+
+    tDeps2.onmouseleave = function() {
+        tAnchor.classList.remove("c-header__gnb__list__item__anchor--active")
+        tList.querySelectorAll(".c-header__gnb__list__item")[tDeps2MenuItemOrgIndex].querySelector("a").classList.add("c-header__gnb__list__item__anchor--active")
+
+        tAnchor.classList.remove("c-header__gnb__list__item__anchor--on")
     }
 }
 
