40025ae66e7bab192197e57b49934881c85a1ba3
이승일 <ever9415@gmail.com>
2023-06-28 16:16:17 +0900

    이승일: Brand GNB 2뎁스 모바일 버전 퍼블리싱 완료

--- lg-global/brand-story/layout.js
+++ lg-global/brand-story/layout.js
@@ -21,6 +21,7 @@ function initCommonHeader() {
         if(window.innerWidth <= 768) {
             deviceWidth = "mobile";
         } else if(window.innerWidth > 768) {
+            cHeader.removeAttribute("style")
             gnb.removeAttribute("style");
             btnGnbBar.forEach(function(item) {
                 item.className = item.className.replace("c-header__btn-gnb__bar c-header__btn-gnb__bar--close","c-header__btn-gnb__bar")
@@ -51,7 +52,7 @@ function initCommonHeader() {
                 item.className = item.className.replace("c-header__btn-gnb__bar c-header__btn-gnb__bar--close","c-header__btn-gnb__bar")
                 gnb.style.transform = "translateY(100vh)";
                 setTimeout(function() {
-                    gnb.style.display = "none"
+                    gnb.removeAttribute("style")
                 },300)
             }
         })
@@ -59,13 +60,11 @@ function initCommonHeader() {
 }
 
 function initGnbDeps2(tAnchor) {
-    if(window.innerWidth <= 768) {
-        return false;
-    }
 
     var tAnchor = tAnchor;
     var tItem = tAnchor.parentNode;
     var tList = tItem.parentNode;
+    var gdItem = tList.getElementsByTagName("li");
     var tDeps2 = tItem.querySelector(".c-header__gnb__list__item__deps2");
     var tDeps2Menu = tDeps2.querySelector(".c-header__gnb__list__item__deps2__menu");
     var tDeps2MenuItem = tDeps2Menu.querySelectorAll(".c-header__gnb__list__item__deps2__menu__item");
@@ -73,6 +72,7 @@ function initGnbDeps2(tAnchor) {
     var tDeps2Visual = tDeps2.querySelector(".c-header__gnb__list__item__deps2__visual");
     var tDeps2VisualItem = tDeps2Visual.querySelectorAll(".c-header__gnb__list__item__deps2__visual__item");
         tDeps2VisualItem[tDeps2MenuItemOrgIndex].classList.add("c-header__gnb__list__item__deps2__visual__item--active");
+    var tDeps2MenuBtnBack = tDeps2.querySelector(".c-header__gnb__list__item__deps2__btn-back")
     var gnbDeps2Bg = document.querySelector(".gnb__deps2__bg");
     var deviceMode = function() {
         if(window.innerWidth > 768) {
@@ -84,9 +84,37 @@ function initGnbDeps2(tAnchor) {
 
     function resizeDeps2Menu() {
         if(deviceMode() == "pc") {
+            tList.style.display = "flex"
             tDeps2.style.width = document.querySelector("body").offsetWidth - 60 + "px"
+
+            if(tAnchor.className.indexOf("--on") > -1) {
+                for (var w = 0; w < gdItem.length; w++) {
+                    if(gdItem[w].className.indexOf("deps2") > -1) continue;
+                    gdItem[w].removeAttribute("style")
+                }
+                tItem.removeAttribute("style")
+                tItem.removeAttribute("style")
+                tAnchor.removeAttribute("style")
+                gnbDeps2Bg.style.display = "block"
+                tDeps2MenuBtnBack.removeAttribute("style")
+            }
         } else if(deviceMode() == "mo") {
-            
+            tList.style.display = "block"
+            tDeps2.style.width = "auto"
+
+            if(tAnchor.className.indexOf("--on") > -1) {
+                for (var x = 0; x < gdItem.length; x++) {
+                    if(gdItem[x].className.indexOf("deps2") > -1) continue;
+                    gdItem[x].style.display = "none"
+                }
+                tItem.style.display = "block"
+                tItem.style.marginTop = "0px"
+                tAnchor.style.display = "none"
+                gnbDeps2Bg.removeAttribute("style")
+                setTimeout(function() {
+                    tDeps2MenuBtnBack.style.opacity = 1
+                },300)
+            }
         }
     }
     resizeDeps2Menu()
@@ -106,7 +134,22 @@ function initGnbDeps2(tAnchor) {
             restItem.querySelector("a").classList.remove("c-header__gnb__list__item__anchor--on")
         })
         tAnchor.classList.add("c-header__gnb__list__item__anchor--on")
-        gnbDeps2Bg.style.display = "block"
+
+        if(deviceMode() == "pc") {
+            gnbDeps2Bg.style.display = "block"
+        } else if(deviceMode() == "mo") {
+            for (var y = 0; y < gdItem.length; y++) {
+                if(gdItem[y].className.indexOf("deps2") > -1) continue;
+                gdItem[y].style.display = "none"
+            }
+            tItem.style.display = "block"
+            tItem.style.marginTop = "0px"
+            tAnchor.style.display = "none"
+            setTimeout(function() {
+                tDeps2MenuBtnBack.style.opacity = 1
+            },100)
+        }
+        
     }
 
     tDeps2MenuItem.forEach(function(t2mi,i) {
@@ -136,6 +179,20 @@ function initGnbDeps2(tAnchor) {
         gnbDeps2Bg.style.display = "none"
 
         tAnchor.classList.remove("c-header__gnb__list__item__anchor--on")
+    }
+
+    tDeps2MenuBtnBack.onclick = function() {
+        tDeps2MenuBtnBack.style.opacity = 0
+        setTimeout(function() {
+            for (var y = 0; y < gdItem.length; y++) {
+                if(gdItem[y].className.indexOf("deps2") > -1) continue;
+                gdItem[y].removeAttribute("style")
+            }
+            tItem.removeAttribute("style")
+            tItem.removeAttribute("style")
+            tAnchor.removeAttribute("style")
+            tAnchor.classList.remove("c-header__gnb__list__item__anchor--on")
+        },100)
     }
 }
 
