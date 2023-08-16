dce2f4c648247fc4233253f56f0daa44315c6147
HJsong <seaalgae.0@gmail.com>
2023-08-16 10:36:05 +0900

    css 공통화 작업중

--- lg-global/brand-visualization/inc/footer.js
+++ lg-global/brand-visualization/inc/footer.js
@@ -1,8 +1,34 @@
 const footer = `
 <div id="footer__wrap" class="c-footer__wrap">
-    <footer id="footer" class="c-footer">
-
-    </footer>
+<footer id="footer" class="c-footer">
+    <div class="footer-top">
+        <ul>
+            <li><a class="icon-sns facebook" href="#"></a></li>
+            <li><a class="icon-sns twitter" href="#"></a></li>
+            <li><a class="icon-sns instagram" href="#"></a></li>
+            <li><a class="icon-sns youtube" href="#"></a></li>
+        </ul>
+    </div>
+    <div class="footer-bottom">
+        <div>
+            <ul class="foot-location">
+                <li><a href="#">Site map</a></li>
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">Legal</a></li>
+                <li><a href="#">CEDOC</a></li>
+                <li><a href="#">SAR</a></li>
+                <li><a href="#">LG Affiliates</a></li>
+            </ul>
+            <p class="c-copyright">
+                Copyright © 2009-2023 LG Electronics. All Rights Reserved
+            </p>
+        </div>
+        <a class="c-hotline" href="#">
+            LG Jeong-Do Management<br>
+            Ethics Hotline
+        </a>
+    </div>
+</footer>
 </div>
 `
 
